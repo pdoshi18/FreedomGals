7f26629b0e125cafd56babfc9400d5535c135067
Brooms404 <70541984+Brooms404@users.noreply.github.com>
2022-05-03 22:27:00 -0500

    Adding comments to read.py

--- read.py
+++ read.py
@@ -4,9 +4,9 @@ import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 
 reader = SimpleMFRC522()
-
+#This was initally used to find the RFID card values so we could finalize the code but now is an extra precaution to check if card scanned properly
 try:
-    print("Waiting for you to scan an RFID sticker/card")
+    print("Waiting for RFID card")
     id = reader.read()[0]
     print("The ID for this card is:", id)
 #uses the RFID scanner to scan the value of the card, which is then attatched with a specific playlist/track/album on spotify
